350848745b3bb315abc4e0de11bed9808e80c215
saber46 <sabermahmud.sourav.7@gmail.com>
2022-04-04 02:26:02 +0600

    Update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var menuBtn = document.querySelectorAll('.menu-body > .btn');
 var menuSub = document.querySelectorAll('.menu-body > .sub-btn');
-
+var btnIcon = document.createElement("i");
+btnIcon.className = 'fa-solid fa-angle-right';
 menuBtn.forEach((btn, i) => {
+    btn.appendChild(btnIcon);
     const sub = menuSub[i];
     btn.addEventListener('click', ()=> {
         sub.classList.toggle('active');
